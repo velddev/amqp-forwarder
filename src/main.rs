218dbe59a5fdfe8c8efec6d41f2f6a714721b9bd
Veld <velddev@gmail.com>
2018-07-06 16:34:33 +0200

    add: test code

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#[macro_use] extern crate serenity;
+
+use serenity::client::Client;
+use serenity::prelude::EventHandler;
+use serenity::framework::standard::StandardFramework;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+
+fn main() {
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("DISCORD_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+    client.with_framework(StandardFramework::new()
+        .configure(|c| c.prefix("~")) // set the bot's prefix to "~"
+        .cmd("ping", ping));
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+command!(ping(_context, message) {
+    let _ = message.reply("Pong!");
+});
